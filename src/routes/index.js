f036ece0f3dc9a9eccc233086e3fa0ff5a4c06c2
Ngọc Khiêm <ngockhiem@Ngoc-Khiem-MacBook-Pro.local>
2023-10-17 21:45:06 +0700

    nhung model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,187 +1,125 @@
-const UserRouter = require('./UserRouter')
-const ProductRouter = require('./ProductRouter')
-const fs = require('fs')
-const base64ToImage = require('base64-to-image')
-let { PythonShell } = require('python-shell')
+const UserRouter = require("./UserRouter");
+const ProductRouter = require("./ProductRouter");
+// const fs = require("fs");
+const base64ToImage = require("base64-to-image");
+let { PythonShell } = require("python-shell");
 
 const routes = (app) => {
-    app.use('/api/user', UserRouter)
-    app.use('/api/product', ProductRouter)
-    
-    const arr = ['']
-    app.post("/save", (req, res) => {
-        // Nhận dữ liệu từ ReactJS
-        const text = req.body.imageBase64 
-        arr.push(req.body.imageBase64)
-        console.log(arr)
-        if(text === arr[0]) {
-            console.log('banggggggggggggggggggg')
-            fs.writeFile('../Clothing_Store/src/components/isLoading.txt',
-                'false', function(err) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('File written false successfully!')
-                }
-            })
-            if(arr.length > 1) {
-                arr.shift()
-            }
-        } else {
-            console.log('khongggggggggggggggggggg')
-            base64ToImage(text.toString(), 'my_images/')
+  app.use("/api/user", UserRouter);
+  app.use("/api/product", ProductRouter);
 
-            const imagee = 'my_images/img.jpeg'
-            let options = {
-                pythonPath: 'python3',
-                args:[imagee],
-            }
+  const arr = [""];
+  app.post("/save", (req, res) => {
+    // Nhận dữ liệu từ ReactJS
+    const text = req.body.imageBase64;
+    base64ToImage(text.toString(), "my_images/");
 
-            fs.writeFile('../Clothing_Store/src/components/isLoading.txt', 
-                'true', function(err) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('File written true successfully!')
-                }
-            })
+    // arr.push(req.body.imageBase64)
+    // console.log(arr)
+    // if(text === arr[0]) {
+    //     console.log('banggggggggggggggggggg')
 
-            PythonShell.run("src/routes/age.py", options, function(err, results) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(results)
-                    fs.writeFile('../Clothing_Store/src/components/age.txt', 
-                        results[0], function(err) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log('File written age successfully!')
-                        }
-                    })
-                }
-            })
+    //     // fs.writeFile('../Clothing_Store/src/components/isLoading.txt',
+    //     //     'false', function(err) {
+    //     //     if (err) {
+    //     //         console.log(err)
+    //     //     } else {
+    //     //         console.log('File written false successfully!')
+    //     //     }
+    //     // })
 
-            PythonShell.run("src/routes/gender.py", options, function(err, results) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(results)
-                    fs.writeFile('../Clothing_Store/src/components/gender.txt', 
-                        results[0], function(err) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log('File written gender successfully!')
-                        }
-                    })
-                }
-            })
+    //     if(arr.length > 1) {
+    //         arr.shift()
+    //     }
+    // } else {
+    //     console.log('khongggggggggggggggggggg')
 
-            PythonShell.run("src/routes/functions.py", options, function(err, results) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(results)
-                    if(results) {
-                        fs.writeFile('../Clothing_Store/src/components/bmi.txt', 
-                        results[0], function(err) {
-                            if (err) {
-                                console.log(err)
-                            } else {
-                                console.log('File written bmi successfully!')
-                                fs.writeFile('../Clothing_Store/src/components/isLoading.txt', 
-                                    'false', function(err) {
-                                    if (err) {
-                                        console.log(err)
-                                    } else {
-                                        console.log('File written false successfully!')
-                                    }
-                                })
-                            }
-                        })
-                    }
-                }
-            })
-            
-            arr.shift()
-        }
+    //     // fs.writeFile('../Clothing_Store/src/components/isLoading.txt',
+    //     //     'true', function(err) {
+    //     //     if (err) {
+    //     //         console.log(err)
+    //     //     } else {
+    //     //         console.log('File written true successfully!')
+    //     //     }
+    //     // })
 
-        // const promise_Age = new Promise((resolve, reject) => {
-        //     PythonShell.run("src/routes/age.py", options, function(err, results) {
-        //         if (err) {
-        //             reject(err)
-        //         } else {
-        //             resolve(results)
-        //         }
-        //     })
-        // })
-            
-        // promise_Age
-        // .then((results) => {
-        //     console.log(results[0])
-        //     fs.writeFile('../Clothing_Store/src/components/age.txt', results[0], function(err) {
-        //         if (err) {
-        //             console.log(err)
-        //         } else {
-        //             console.log('File written age successfully!')
-        //         }
-        //     })
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
+    //     // PythonShell.run("src/routes/age.py", options, function(err, results) {
+    //     //     if (err) {
+    //     //         console.log(err)
+    //     //     } else {
+    //     //         console.log(results)
+    //     //         fs.writeFile('../Clothing_Store/src/components/age.txt',
+    //     //             results[0], function(err) {
+    //     //             if (err) {
+    //     //                 console.log(err)
+    //     //             } else {
+    //     //                 console.log('File written age successfully!')
+    //     //             }
+    //     //         })
+    //     //     }
+    //     // })
 
-    //     const promise_BMI = new Promise((resolve, reject) => {
-    //       PythonShell.run("src/routes/functions.py", options, function(err, results) {
-    //           if (err) {
-    //               reject(err)
-    //           } else {
-    //               resolve(results)
-    //           }
-    //       })
-    //   })
-          
-    //   promise_BMI
-    //   .then((results) => {
-    //       console.log(results[0])
-    //       fs.writeFile('../Clothing_Store/src/components/bmi.txt', results[0], function(err) {
-    //           if (err) {
-    //               console.log(err)
-    //           } else {
-    //               console.log('File written bmi successfully!')
-    //           }
-    //       })
-    //   })
-    //   .catch((err) => {
-    //       console.log(err)
-    //   })
-      
-    //   const promise_Gender = new Promise((resolve, reject) => {
-    //       PythonShell.run("src/routes/gender.py", options, function(err, results) {
-    //           if (err) {
-    //               reject(err)
-    //           } else {
-    //               resolve(results)
-    //           }
-    //       })
-    //   })
-          
-    //   promise_Gender
-    //   .then((results) => {
-    //       console.log(results[0])
-    //       fs.writeFile('../Clothing_Store/src/components/gender.txt', results[0], function(err) {
-    //           if (err) {
-    //               console.log(err)
-    //           } else {
-    //               console.log('File written gender successfully!')
-    //           }
-    //       })
-    //   })
-    //   .catch((err) => {
-    //       console.log(err)
-    //   })
-    })
-}
+    //     // PythonShell.run("src/routes/gender.py", options, function(err, results) {
+    //     //     if (err) {
+    //     //         console.log(err)
+    //     //     } else {
+    //     //         console.log(results)
+    //     //         fs.writeFile('../Clothing_Store/src/components/gender.txt',
+    //     //             results[0], function(err) {
+    //     //             if (err) {
+    //     //                 console.log(err)
+    //     //             } else {
+    //     //                 console.log('File written gender successfully!')
+    //     //             }
+    //     //         })
+    //     //     }
+    //     // })
 
-module.exports = routes
+    //     // PythonShell.run("src/routes/functions.py", options, function(err, results) {
+    //     //     if (err) {
+    //     //         console.log(err)
+    //     //     } else {
+    //     //         console.log(results)
+    //     //         if(results) {
+    //     //             fs.writeFile('../Clothing_Store/src/components/bmi.txt',
+    //     //             results[0], function(err) {
+    //     //                 if (err) {
+    //     //                     console.log(err)
+    //     //                 } else {
+    //     //                     console.log('File written bmi successfully!')
+    //     //                     fs.writeFile('../Clothing_Store/src/components/isLoading.txt',
+    //     //                         'false', function(err) {
+    //     //                         if (err) {
+    //     //                             console.log(err)
+    //     //                         } else {
+    //     //                             console.log('File written false successfully!')
+    //     //                         }
+    //     //                     })
+    //     //                 }
+    //     //             })
+    //     //         }
+    //     //     }
+    //     // })
+
+    //     arr.shift()
+    // }
+  });
+
+  app.get("/save", async (req, res) => {
+    const imagee = "my_images/img.jpeg";
+    let options = {
+      pythonPath: "python3",
+      args: [imagee],
+    };
+    PythonShell.run("src/routes/model.py", options, function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(results);
+        res.json(results);
+      }
+    });
+  });
+};
+
+module.exports = routes;
