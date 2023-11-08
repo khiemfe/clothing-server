15a56887d6a4f00f2a7506a5eaa58521435648d0
Ngọc Khiêm <ngockhiem@Ngoc-Khiem-MacBook-Pro.local>
2023-11-08 23:42:21 +0700

    search ok

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,15 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const ProductControllers = require("../controllers/ProductControllers");
+const {
+  authMiddleware,
+  authUserMiddleware,
+} = require("../middleware/authMiddleware");
 
-const express = require('express')
-const router = express.Router()
-const ProductControllers = require('../controllers/ProductControllers')
-const { authMiddleware, authUserMiddleware } = require('../middleware/authMiddleware')
+router.post("/create", authMiddleware, ProductControllers.createProduct);
+router.put("/update/:id", authMiddleware, ProductControllers.updateProduct);
+router.get("/get-details/:id", ProductControllers.getdetailsProduct);
+router.delete("/delete/:id", authMiddleware, ProductControllers.deleteProduct);
+router.get("/get-all", ProductControllers.getAllProduct);
+router.post(
+  "/delete-many",
+  authMiddleware,
+  ProductControllers.deleteManyProduct
+);
 
-router.post('/create', authMiddleware, ProductControllers.createProduct)
-router.put('/update/:id', authMiddleware, ProductControllers.updateProduct)
-router.get('/get-details/:id', ProductControllers.getdetailsProduct)
-router.delete('/delete/:id', authMiddleware, ProductControllers.deleteProduct)
-router.get('/get-all', ProductControllers.getAllProduct)
-router.post('/delete-many', authMiddleware, ProductControllers.deleteManyProduct)
-
-module.exports = router
-
+module.exports = router;
