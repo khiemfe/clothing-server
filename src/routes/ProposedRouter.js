5c6398cc00df02bb55519d4e20ae08ae58060ba2
Ngọc Khiêm <ngockhiem@Ngoc-Khiem-MacBook-Pro.local>
2023-11-30 20:31:51 +0700

    lan6

--- src/routes/ProposedRouter.js
+++ src/routes/ProposedRouter.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const base64ToImage = require("base64-to-image");
 
 router.post("/proposed", (req, res) => {
-  const text = req.body.imageBase64;
-  base64ToImage(text.toString(), "my_images");
+  try {
+    const text = req.body.imageBase64;
+    base64ToImage(text.toString(), "my_images");
+    return res.status(200).json({
+      status: "OK",
+      message: "Chay ok vao day",
+    });
+  } catch {
+    return res.status(404).json({
+      message: "loi img",
+    });
+  }
 });
 module.exports = router;
